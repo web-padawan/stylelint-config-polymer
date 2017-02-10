3aaceab08165a2c5485968b7d337fba874ebab15
web-padawan <serguey.kulikov@gmail.com>
2017-02-10 11:40:13 +0200

    Extend from standard config. Add rules for :host-context and ::slotted

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,137 +1,111 @@
-import config from "../"
-import stylelint from "stylelint"
-import test from "ava"
+"use strict"
+
+const config = require("../")
+const stylelint = require("stylelint")
 
 const validCss = (
-`@import url(x.css);
-@import url(y.css);
-
-/**
- * Multi-line comment
- */
-
+`
 :host {
   display: block;
-  padding: 8px 0;
-
-  @apply(--paper-input-container);
 }
 
-.input-content ::content input,
-.input-content ::content textarea,
-.input-content ::content iron-autogrow-textarea,
-.input-content ::content .paper-input-input {
-  position: relative; /* to make a stacking context */
-  outline: none;
-  box-shadow: none;
-  padding: 0;
+:host-context([dir="rlt"]) {
+  float: right;
+}
+
+.input-content ::content > paper-input {
   width: 100%;
-  max-width: 100%;
-  background: transparent;
-  border: none;
-  color: var(--paper-input-container-input-color, --primary-text-color);
-  -webkit-appearance: none;
-  text-align: inherit;
-
-  @apply(--paper-font-subhead);
-  @apply(--paper-input-container-input);
 }
 
-.selector-1,
-.selector-2,
-.selector-3[type="text"] {
-  background: linear-gradient(#fff, rgba(0, 0, 0, 0.8));
-  box-sizing: border-box;
-  display: block;
-  color: #333;
+:host ::shadow .input {
+  color: red;
 }
 
-.selector-a,
-.selector-b:not(:first-child) {
-  padding: 10px !important;
-  top: calc(calc(1em * 2) / 3);
-}
-
-.selector-x { width: 10%; }
-.selector-y { width: 20%; }
-.selector-z { width: 30%; }
-
-/* Single-line comment */
-
-@media (min-width >= 60em) {
-  .selector {
-    /* Flush to parent comment */
-    transform: translate(1, 1) scale(3);
-  }
-}
-
-@media (min-orientation: portrait), projection and (color) {
-  .selector-i + .selector-ii {
-    background: color(rgb(0, 0, 0) lightness(50%));
-    font-family: helvetica, "arial black", sans-serif;
-  }
-}
-
-/* Flush single line comment */
-@media
-  screen and (min-resolution: 192dpi),
-  screen and (min-resolution: 2dppx) {
-  .selector {
-    background-image:
-      repeating-linear-gradient(
-        -45deg,
-        transparent,
-        #fff 25px,
-        rgba(255, 255, 255, 1) 50px
-      );
-    margin: 10px;
-    margin-bottom: 5px;
-    box-shadow:
-      0 1px 1px #000,
-      0 1px 0 #fff,
-      2px 2px 1px 1px #ccc inset;
-    height: 10rem;
-  }
-
-  /* Flush nested single line comment */
-  .selector::after {
-    content: '→';
-    background-image: url(x.svg);
-  }
+.wrapper ::slotted(.icon) {
+  color: #000;
 }
 
 `)
 
 const invalidCss = (
-`a {
-  top: .2em;
+`1-no-way {
+  color: red;
 }
 
 `)
 
-test("no warnings with valid css", t => {
-  return stylelint.lint({
-    code: validCss,
-    config: config,
+describe("flags no warnings with valid css", () => {
+  let result
+
+  beforeEach(() => {
+    result = stylelint.lint({
+      code: validCss,
+      config,
+    })
   })
-  .then(data => {
-    const { errored, results } = data
-    const { warnings } = results[0]
-    t.falsy(errored, "no errored")
-    t.is(warnings.length, 0, "flags no warnings")
+
+  it("did not error", () => {
+    return result.then(data => (
+      expect(data.errored).toBeFalsy()
+    ))
+  })
+
+  it("flags no warnings", () => {
+    return result.then(data => (
+      expect(data.results[0].warnings.length).toBe(0)
+    ))
   })
 })
 
-test("a warning with invalid css", t => {
-  return stylelint.lint({
-    code: invalidCss,
-    config: config,
+describe("flags warnings with invalid css", () => {
+  let result
+
+  beforeEach(() => {
+    result = stylelint.lint({
+      code: invalidCss,
+      config,
+    })
   })
-  .then(data => {
-    const { errored, results } = data
-    const { warnings } = results[0]
-    t.truthy(errored, "errored")
-    t.is(warnings.length, 1, "flags one warning")
-    t.is(warnings[0].text, "Expected a leading zero (number-leading-zero)", "correct warning text")
+
+  it("did error", () => {
+    return result.then(data => (
+      expect(data.errored).toBeTruthy()
+    ))
+  })
+
+  it("flags one warning", () => {
+    return result.then(data => (
+      expect(data.results[0].warnings.length).toBe(1)
+    ))
+  })
+
+  it("correct warning text", () => {
+    return result.then(data => (
+      expect(data.results[0].warnings[0].text).toBe("Unexpected unknown type selector \"1-no-way\" (selector-type-no-unknown)")
+    ))
+  })
+
+  it("correct rule flagged", () => {
+    return result.then(data => (
+      expect(data.results[0].warnings[0].rule).toBe("selector-type-no-unknown")
+    ))
+  })
+
+  it("correct severity flagged", () => {
+    return result.then(data => (
+      expect(data.results[0].warnings[0].severity).toBe("error")
+    ))
+  })
+
+  it("correct line number", () => {
+    return result.then(data => (
+      expect(data.results[0].warnings[0].line).toBe(1)
+    ))
+  })
+
+  it("correct column number", () => {
+    return result.then(data => (
+      expect(data.results[0].warnings[0].column).toBe(1)
+    ))
   })
 })
